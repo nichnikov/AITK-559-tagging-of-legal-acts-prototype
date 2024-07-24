d5b914ce1ad2c1eabc0d8eb3f59b96037c8dd780
nichnikov <a.nichnikov@gmail.com>
2024-07-24 21:45:27 +0300

    ref

--- s3_get_acts.py
+++ s3_get_acts.py
@@ -20,47 +20,24 @@ print(files_pathes_df["path_file_in_baquet_s3"])
 
 
 
-for pdf_p in files_pathes_df["path_file_in_baquet_s3"].to_list()[:20]:
-    pdf_path = s3_url + pdf_p
+# for d in files_pathes_df["path_file_in_baquet_s3"].to_list()[:5]:
+for d in files_pathes_df.to_dict(orient="records")[:5]:
+    pdf_path = s3_url + d["path_file_in_baquet_s3"]
     pdf_path = re.sub("\s+", "", pdf_path)
     print("pdf path:", pdf_path)
     response = requests.get(pdf_path)
-    print("GET:", response)
-    
-    
-    # print(response.content)
-    # response.close()
-    # del response
-    '''    
+       
     try:
         response = requests.get(pdf_path)
-        print(response.content)
+
         output = io.BytesIO(response.content)
-        # output.write(response.content)
+
         pages_texts = text_extract_from_page(output, 5)
-        # output.close()
-        # del output
-        
+
+        doc = ""
         for p_tx in pages_texts:
-            print(p_tx)
+            doc += p_tx
+        print(re.sub("\n|\s+", " ", doc), "\n")    
+
     except:
         pass
-    '''
-
-
-
-
-# pdf_path = "https://s3-documents.prod.dataplatform.aservices.tech/dppdfpravoru30/3553a013-7787-49b5-ab50-d13c600dc08a.pdf"
-pdf_path = "https://s3-documents.prod.dataplatform.aservices.tech/dppdfpravoru36/112b08ba-2244-4f0c-ba50-f67d4019b729.pdf"
-output = io.BytesIO()
-response = requests.get(pdf_path)
-output.write(response.content)
-pages_texts = text_extract_from_page(output, 5)
-
-for p_tx in pages_texts:
-    print(p_tx)
-output.close()
-
-# pdf_path = "https://s3-documents.prod.dataplatform.aservices.tech/dppdfpravoru264/e5635e72-829d-44f0-b1b7-ae8ef835f045.pdf" 
-
-
