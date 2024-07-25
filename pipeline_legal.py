00fe69b3853a01126de8f0389f093b9a9a1a8161
nichnikov <a.nichnikov@gmail.com>
2024-07-25 18:30:29 +0300

    ref

--- pipeline_legal.py
+++ pipeline_legal.py
@@ -25,7 +25,7 @@ with open(os.path.join("data", "prompts.json"), "r") as jf:
 
 
 temp = 0.5
-for d in files_pathes_df.to_dict(orient="records")[:5]:
+for d in files_pathes_df.to_dict(orient="records")[:25]:
     pdf_path = s3_url + d["path_file_in_baquet_s3"]
     pdf_path = re.sub("\s+", "", pdf_path)
 
@@ -44,36 +44,23 @@ for d in files_pathes_df.to_dict(orient="records")[:5]:
     doc_text = re.sub("\n|\s+", " ", doc)
 
     for intent in prompts_json:
-        act_fragment = re.findall(prompts_json[intent]["reg"], doc_text, re.IGNORECASE)
-        if act_fragment:
-            message = " ".join([prompts_json[intent]["prompt_start"], act_fragment[0], prompts_json[intent]["prompt_finish"]])
-            js_data = {
-                        "temperature": temp,
-                        "model": "openchat_3.5",
-                        "messages": [{"role": "user", "content": str(message)}]
-                        }
+        if intent == "claim":
+            act_fragment = re.findall(prompts_json[intent]["reg"], doc_text, re.IGNORECASE)
+            if act_fragment:
+                message = " ".join([prompts_json[intent]["prompt_start"], act_fragment[0], prompts_json[intent]["prompt_finish"]])
+                js_data = {
+                            "temperature": temp,
+                            "model": "openchat_3.5",
+                            "messages": [{"role": "user", "content": str(message)}]
+                            }
 
-            r = requests.post(LLM_URL, json=js_data)
-            res_dct = r.json()
-            
-            result_text = res_dct["choices"][0]["message"]["content"]
-            
-            print("\n", intent, "result text:", result_text)
-            
-        else:
-            print("НЕТ ФРАГМЕНТА")
-            
-    
-    js_data = {
-        "temperature": temp,
-        "model": "openchat_3.5",
-        "messages": [{"role": "user", "content": str(message)}]
-    }
-
-    r = requests.post(LLM_URL, json=js_data)
-    res_dct = r.json()
-    
-    result_text = re.sub("Короткий текст:", "", res_dct["choices"][0]["message"]["content"])
-    
-    print("\n", message)
-    print("\nresult text:", result_text)
+                r = requests.post(LLM_URL, json=js_data)
+                res_dct = r.json()
+                
+                result_text = res_dct["choices"][0]["message"]["content"]
+                print("\n", "интент:", intent)                
+                print(message, "\n", "result:", result_text)
+                
+            else:
+                print("НЕТ ФРАГМЕНТА")
+                
